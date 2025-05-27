080eb67ebc0b68b7beb5d7b0de03eb1e746c49a7
RH <ryunh@khu.ac.kr>
2025-05-27 13:43:51 +0900

    계산기 메인 함수 수정

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from divide import divide
 
 
 def calculator():
-    a = float(input("첫 번째 숫자: "))
-    b = float(input("두 번째 숫자: "))
+    a = float(input("첫 번째 숫자 입력: "))
+    b = float(input("두 번째 숫자 입력: "))
     op = input("연산자 (+, -, *, /): ")
     operations = {
         '+': add,
@@ -17,11 +17,11 @@ def calculator():
     if op in operations:
         try:
             result = operations[op](a, b)
-            print("결과:", result)
+            print("결과 입력:", result)
         except Exception as e:
             print("오류:", e)
     else:
-        print("올바르지 않은 연산자입니다.")
+        print("올바르지 않은 연산자입니다!!")
 
 
 if __name__ == "__main__":
